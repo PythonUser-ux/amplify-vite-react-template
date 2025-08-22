ce4d04acf397bfea886d6cae4dcfec8e8951352b
Andrea Virgillito <andr_9v9@hotmail.com>
2025-08-22 22:14:01 +0200

    added phone number

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,18 @@
+import { defineAuth } from '@aws-amplify/backend';
+
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+    phone: true,
+  },
+  userAttributes: {
+    phoneNumber: {
+        required: true,
+        mutable: true,
+    },
+    email: {
+        required: true,
+        mutable: true,
+    },
+    }
+});
