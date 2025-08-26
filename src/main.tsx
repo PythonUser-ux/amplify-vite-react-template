b5da6688d6f181a060c6a76439a5f82d7eac0a23
Andrea Virgillito <andr_9v9@hotmail.com>
2025-08-26 20:18:14 +0200

    added google auth2

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,28 @@ import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
 
-Amplify.configure(outputs);
+// Amplify.configure(outputs);
+Amplify.configure({
+  ...outputs,
+  Auth: {
+    Cognito: {
+      // <-- force your new pool/client here
+      userPoolId: "eu-north-1_PxOyzFtjR",
+      userPoolClientId: "4qcm8ebs4kak0j955hifcces4o",
+      loginWith: {
+        email: true,
+        oauth: {
+          domain: "il-social-utile.auth.eu-north-1.amazoncognito.com",
+          redirectSignIn: ["https://main.d4o7j5d8taluc.amplifyapp.com/"],
+          redirectSignOut: ["https://main.d4o7j5d8taluc.amplifyapp.com/"],
+          responseType: "code",
+          scopes: ["openid", "email", "profile"],
+        },
+      },
+    },
+  },
+});
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
